4743ae2897a135327220b0e3bf3578f14d875eba
zoosjuice <dkdejesus@gmail.com>
2014-08-18 22:02:50 -0700

    Adding Bear Node

---
+++ WatermelonNinja/WNBear.m
@@ -0,0 +1,28 @@
+//
+//  WNBear.m
+//  WatermelonNinja
+//
+//  Created by David de Jesus on 8/18/14.
+//  Copyright (c) 2014 UNMD76. All rights reserved.
+//
+
+#import "WNBear.h"
+
+@implementation WNBear
+
++(WNBear *)addToScene:(SKScene *)scene
+{
+    WNBear *bear = [[WNBear alloc] initWithTexture:[SKTexture textureWithImageNamed:@"bear"]];
+    
+    bear.xScale = bear.yScale = 0.5;
+    
+    bear.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:bear.size.width*0.4];
+    
+    
+    [scene addChild:bear];
+    
+    return bear;
+}
+
+
+@end
