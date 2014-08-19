2641caea126d7bf112b99e57196d37900cc01562
Jon Como <jon@underground.net>
2014-08-18 22:25:45 -0700

    played splat sound

--- WatermelonNinja/WNMyScene.m
+++ WatermelonNinja/WNMyScene.m
@@ -60,6 +60,9 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     isSlashing = YES;
+    
+    touchLocation = [[touches anyObject] locationInNode:self];
+    lastTouch = touchLocation;
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
@@ -135,6 +138,8 @@
             [self addSlicesToPoint:melon.position];
             
             [self addParticlesToPoint:melon.position];
+            
+            [self runAction:[SKAction playSoundFileNamed:@"splat.wav" waitForCompletion:NO]];
             
             screenShake += 8;
         }
